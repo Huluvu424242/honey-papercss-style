204c1352cb921fd68ae0e3ba0eeaf1cc729f7cce
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-10-02 19:29:57 +0200

    [#4-styletag] prepare release
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/components/honey-papercss-style/honey-papercss-style.tsx
+++ src/components/honey-papercss-style/honey-papercss-style.tsx
@@ -2,10 +2,26 @@ import {Component, getAssetPath, h} from '@stencil/core';
 
 @Component({
   tag: 'honey-papercss-style',
-  styleUrl: 'honey-papercss-style.css',
   assetsDirs: ['assets']
 })
 export class HoneyPapercssStyle {
+
+  public connectedCallback() {
+    // add class paper to body
+    let classes: string = document.body.getAttribute("class");
+    if(classes === null) classes = "paper";
+    if (classes.search("paper") < 0) {
+      classes += " paper";
+    }
+    document.body.setAttribute("class", classes);
+  }
+
+  public disconnectedCallback() {
+    // remove class paper from body
+    let classes: string = document.body.getAttribute("class");
+    classes = classes.replace("paper", "");
+    document.body.setAttribute("class", classes);
+  }
 
   render() {
     const stylePath: string = getAssetPath('./assets/paper.min.css');
